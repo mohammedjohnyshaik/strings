255c0413c6981165b19bc28c71042881d6179cc6
mohammedjohnysk <mohammedjohnysk@gmail.com>
2023-09-22 21:27:10 -0500

    for and if loop

---
+++ Longestsubstring_4.py
@@ -0,0 +1,12 @@
+s = "abcabcbb"
+max_length = 0
+start = 0
+char_index = {}
+
+for end in range(len(s)):
+    if s[end] in char_index and char_index[s[end]] >= start:
+        start = char_index[s[end]] + 1
+    char_index[s[end]] = end
+    max_length = max(max_length, end - start + 1)
+
+print(max_length)
